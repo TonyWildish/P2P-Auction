4bda6ade7901eaf5d9084d91415e3a9e0d283b46
Tony Wildish <Tony@Wildish.eu>
2015-05-11 17:16:47 +0200

    more updates

--- Paper/make.sh
+++ Paper/make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./clean.sh
 
-pdflatex 'Bandwidth-sharing in LHCONE: an analysis of the problem.tex'
-pdflatex 'Bandwidth-sharing in LHCONE: an analysis of the problem.tex'
+pdflatex 'Bandwidth-sharing in LHCONE: an analysis of the problem.tex' && \
+pdflatex 'Bandwidth-sharing in LHCONE: an analysis of the problem.tex' && \
 open     'Bandwidth-sharing in LHCONE: an analysis of the problem.pdf'
